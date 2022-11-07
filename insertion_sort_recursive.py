d9ff41425375df8dff3478fe69cffdd42033685e
Nitin Sai <nitin41202@gmail.com>
2022-11-07 08:01:12 +0530

    Add files via upload

---
+++ insertion_sort_recursive.py
@@ -0,0 +1,29 @@
+
+
+def insertionSortRecursive(arr,n):
+	# base case
+	if n<=1:
+		return
+	
+	insertionSortRecursive(arr,n-1)
+	'''Insert last element at its correct position
+		in sorted array.'''
+	last = arr[n-1]
+	j = n-2
+	
+	while (j>=0 and arr[j]>last):
+		arr[j+1] = arr[j]
+		j = j-1
+
+	arr[j+1]=last
+
+def printArray(arr,n):
+	for i in range(n):
+		print(arr[i],end=" ")
+
+
+arr = [12,11,13,5,6]
+n = len(arr)
+insertionSortRecursive(arr, n)
+printArray(arr, n)
+
